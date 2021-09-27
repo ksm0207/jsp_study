492d7b03871320b1a23d48fab803bc2d83a44061
ksm0207 <ksm03071@naver.com>
2021-09-27 16:53:03 +0900

    board db object

---
+++ Board/src/vo/TestBoardVO.java
@@ -0,0 +1,52 @@
+package vo;
+
+public class TestBoardVO {
+	
+	String idx;
+	String title;
+	String userID;
+	String datetime;
+	String content;
+	String available;
+	
+	
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAvailable() {
+		return available;
+	}
+	public void setAvailable(String available) {
+		this.available = available;
+	}
+	
+	
+
+}
